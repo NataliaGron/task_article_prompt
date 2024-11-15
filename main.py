ccd8725751d8830f50f9c8ae8c14a386bdc7d52a
Natalia Gron <nanusza@gmail.com>
2024-11-15 22:55:47 +0100

    Add html template

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ def generate_html_article(article_text):
     prompt = f"""
         Przekształć poniższy artykuł do struktury HTML.
         Użyj odpowiednich tagów HTML do strukturyzacji treści.
+        Kod powinien znajdować się w tagu <article>.
         Zidentyfikuj miejsca, gdzie warto wstawić grafiki i oznacz je tagiem <img src="image_placeholder.jpg" alt="<prompt>">.
         Niech prompt będzie bardzo dokładny, aby można go było użyć do wygenerowania grafiki.
+        Najlepiej, aby zaczynał się od słów: "Ilustracja przedstawiająca".
         Dodaj podpisy pod obrazkami z użyciem tagu <figcaption>.
         Proszę, aby wynik zawierał tylko część HTML, bez nagłówków <html>, <head> czy <body>.
         Wygeneruj to jako plain text a nie w bloku code.
